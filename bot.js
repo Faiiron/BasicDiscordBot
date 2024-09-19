4715926725930d01bb48ce62c0b1cb9add340fa5
Fairon <161167741+Faiiron@users.noreply.github.com>
2024-09-19 21:53:28 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,24 @@
+// requeriments
+const Discord = require('discord.js');
+
+// define client
+const Client = new Discord.Client({
+    intents: 34615
+
+})
+
+// content
+Client.on('ready', async ( client ) => {
+    console.log('HardBot is Ready!')
+    }   
+)
+
+Client.on('messageCreate', async ( message ) => {
+    if(message.author.bot) return;
+    if(message.content.startsWith('Hola')) message.channel.send('Buenas!')
+
+    }
+);
+
+// conection
+Client.login('YOUR_BOT_TOKEN')
